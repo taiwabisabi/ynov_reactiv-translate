1f8e51d0c955ef149450ee3a4e7319080b471fdd
Tai <htjpascal@gmail.com>
2020-01-16 00:54:19 +0100

    Add Audio-Av & Config Audio Recording & Convert Base64

--- App.js
+++ App.js
@@ -1,13 +1,130 @@
 import React from 'react';
-import { StyleSheet, Text, View } from 'react-native';
+import { StyleSheet, Text, View, Button } from 'react-native';
 
-export default function App() {
-  return (
-    <View style={styles.container}>
-      <Text>Open up App.js to start working on your app!</Text>
-    </View>
-  );
+import { Audio } from 'expo-av';
+import * as Permissions from 'expo-permissions';
+import * as FileSystem from 'expo-file-system';
+
+class App extends React.Component {
+  constructor(props) {
+    super(props);
+    this.recording = null;
+    this.sound = null;
+    this.state = {
+      haveRecordingPermissions: false,
+      isRecording: false,
+      audioString: null,
+      muted: false,
+      shouldCorrectPitch: true,
+      volume: 1.0,
+      rate: 1.0,
+      isPlaying: false,
+    }
+  }
+
+  async beginRecording() {
+    const recording = new Audio.Recording();
+    try {
+      await Audio.setAudioModeAsync({
+        allowsRecordingIOS: true,
+        interruptionModeIOS: Audio.INTERRUPTION_MODE_IOS_DO_NOT_MIX,
+        playsInSilentModeIOS: true,
+        shouldDuckAndroid: true,
+        interruptionModeAndroid: Audio.INTERRUPTION_MODE_ANDROID_DO_NOT_MIX,
+        playThroughEarpieceAndroid: false,
+        staysActiveInBackground: true,
+      });
+      await recording.prepareToRecordAsync(Audio.RECORDING_OPTIONS_PRESET_HIGH_QUALITY);
+      this.recording = recording;
+      console.log(this.recording);
+      await this.recording.startAsync();
+      this.setState({
+        isRecording: true,
+      });
+    } catch (error) {
+      console.log(error);
+    }
+  }
+
+  async stopRecording() {
+    this.setState({
+      isRecording: false,
+    });
+    try {
+      await this.recording.stopAndUnloadAsync();
+    } catch (error) {
+      console.log(error);
+    }
+    const audioString = await FileSystem.readAsStringAsync(this.recording.getURI(), { encoding: FileSystem.EncodingType.Base64 });
+    console.log('audio', audioString);
+    this.setState({
+      audioString
+    });
+    await Audio.setAudioModeAsync({
+      allowsRecordingIOS: false,
+      interruptionModeIOS: Audio.INTERRUPTION_MODE_IOS_DO_NOT_MIX,
+      playsInSilentModeIOS: true,
+      playsInSilentLockedModeIOS: true,
+      shouldDuckAndroid: true,
+      interruptionModeAndroid: Audio.INTERRUPTION_MODE_ANDROID_DO_NOT_MIX,
+      playThroughEarpieceAndroid: false,
+      staysActiveInBackground: true,
+    });
+    const { sound } = await this.recording.createNewLoadedSoundAsync(
+      {
+        isLooping: true,
+        isMuted: this.state.muted,
+        volume: this.state.volume,
+        rate: this.state.rate,
+        shouldCorrectPitch: this.state.shouldCorrectPitch,
+      },
+    );
+    this.sound = sound;
+  }
+
+  onPlayPausePressed = () => {
+    if (this.sound != null) {
+      if (this.state.isPlaying) {
+        this.sound.pauseAsync();
+        this.setState({
+          isPlaying: false
+        });
+      } else {
+        this.sound.playAsync();
+        this.setState({
+          isPlaying: true
+        });
+      }
+    }
+  };
+
+  async componentDidMount() {
+    const res = await Permissions.askAsync(Permissions.AUDIO_RECORDING);
+    this.setState({
+      haveRecordingPermissions: res.status === 'granted',
+    });
+  }
+
+  render() {
+    return (
+      <View style={styles.container}>
+        <Button
+          title="Begin"
+          onPress={() => this.beginRecording()} />
+
+        <Button
+          title="Stop"
+          onPress={() => this.stopRecording()} />
+
+        <Button
+          title="Play / Pause"
+          onPress={() => this.onPlayPausePressed()} />
+      </View>
+    );
+  }
 }
+
+export default App
 
 const styles = StyleSheet.create({
   container: {
