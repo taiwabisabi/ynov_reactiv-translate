a0e556ea6379b596dafa5991e61fd04642eb55bd
Valentin Leymonie <valentin.leymonie.es@gmail.com>
2020-01-21 18:01:18 +0100

    optimize history

--- screens/HistoryScreen.js
+++ screens/HistoryScreen.js
@@ -1,24 +1,35 @@
 import React, { Component } from 'react';
-import { StyleSheet, Text, View, ScrollView } from 'react-native';
-import { withTheme, Appbar, Title, List} from 'react-native-paper';
+import { StyleSheet, View, ScrollView } from 'react-native';
+import { withTheme, Appbar, Title, List, Button, Dialog, Portal, RadioButton, Text} from 'react-native-paper';
 import styles from '../styles';
 import { connect } from "react-redux";
 
 class HistoryScreen extends Component {
-    generateListItem = () => {
-        return this.props.history.map((e, index) => {
-            return <List.Item
-                key={index}
-                titleStyle={[ {color: this.props.theme.colors.primary} ]}
-                title={e.from.text}
-                descriptionNumberOfLines={4}
-                description={e.to.text +'\n'+ e.from.lang.slice(0, 2) + ' -> ' + e.to.lang.slice(0, 2) + '\n' + e.dateTimeTranslation}
-                left={() => <List.Icon icon="translate" />}
-            />
+    state = {
+        visible: false,
+        value: '10'
+    };
+
+    _showDialog = () => this.setState({ visible: true });
+    _hideDialog = () => this.setState({ visible: false });
+
+    _generateListItem = () => {
+        let history = this.props.history.slice().reverse();
+        return history.map((e, index) => {
+            if(index < this.state.value)
+                return <List.Item
+                    key={index}
+                    titleStyle={[ {color: this.props.theme.colors.primary} ]}
+                    title={e.from.text}
+                    descriptionNumberOfLines={4}
+                    description={e.to.text +'\n'+ e.from.lang.slice(0, 2) + ' -> ' + e.to.lang.slice(0, 2) + '\n' + e.dateTimeTranslation}
+                    left={() => <List.Icon icon="translate" />}
+                />
         });
     }
 
     render() {
+        console.log(this.state)
         return (
             <View
                 style={[
@@ -28,10 +39,54 @@ class HistoryScreen extends Component {
                 <Appbar style={styles.top}>
                     <Title>History</Title>
                 </Appbar>
-                <List.Section style={[styles.margin, { paddingHorizontal: 10 }, {marginBottom: 50}]}>
+                <View>
+                    <Button onPress={this._showDialog}>Nombres de résultats</Button>
+
+                    <Portal>
+                        <Dialog visible={this.state.visible} onDismiss={this._hideDialog}>
+                            <Dialog.Title style={[
+                                { color: this.props.theme.colors.primary }
+                            ]}>Combien de résultats souhaitez-vous afficher ?</Dialog.Title>
+
+                            <Dialog.Content>
+                                <RadioButton.Group
+                                    onValueChange={value => this.setState({ value })}
+                                    value={this.state.value}
+                                >
+                                    <View>
+                                        <Text>10</Text>
+                                        <RadioButton value='10' />
+                                    </View>
+                                    <View>
+                                        <Text>20</Text>
+                                        <RadioButton value='20' />
+                                    </View>
+                                    <View>
+                                        <Text>30</Text>
+                                        <RadioButton value='30' />
+                                    </View>
+                                    <View>
+                                        <Text>40</Text>
+                                        <RadioButton value='40' />
+                                    </View>
+                                    <View>
+                                        <Text>50</Text>
+                                        <RadioButton value='50' />
+                                    </View>
+                                </RadioButton.Group>
+                            </Dialog.Content>
+
+                            <Dialog.Actions>
+                                <Button onPress={this._hideDialog}>Done</Button>
+                            </Dialog.Actions>
+                        </Dialog>
+                    </Portal>
+
+                </View>
+                <List.Section style={[styles.margin, { paddingHorizontal: 10 }, {marginBottom: 100}]}>
                     <List.Subheader>Mes dernières traductions</List.Subheader>
                     <ScrollView>
-                        {this.generateListItem()}
+                        {this._generateListItem()}
                     </ScrollView>
                 </List.Section>
             </View>
